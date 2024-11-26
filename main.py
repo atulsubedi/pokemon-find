a245f2726df2572eacccc3ac50058495115fc09f
atul subedi <atul.work15@gmail.com>
2024-11-26 11:23:44 +0545

    update search query

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import csv
 import argparse
 
+# def openAndRead(file):
+#     with open(file, mode='r', newline='') as f:
+#         reader = csv.DictReader(f)
 
-
+def convertFirstLetterToSmall(dict_list):
+    for dict in dict_list:
+        for key in dict:
+            dict[key] = dict[key].lower()
+    return dict_list
 
 def readCsv(file_path):
     data = []
@@ -17,23 +24,24 @@ def printValue(data, column_name, value):
         if row.get(column_name) == value:
             print(row['Name'])
 
-
 def main():
 
     parser = argparse.ArgumentParser(description="process a csv file and ask questions.")
     parser.add_argument('file', type=str, help='path to the csv file')
     args = parser.parse_args()
 
-    data = readCsv(args.file)
+    data = convertFirstLetterToSmall(readCsv(args.file))
+    print(data)
 
     while True:
-        column_name = input("enter the column name (or'exit' to quit):").strip()
+        column_name = input("enter the column name (or'exit' to quit):")
 
         if column_name.lower() == 'exit':
             break
 
-        value = input(f"enter the value to search for in column '{column_name}': ").strip()
+        value = input(f"enter the value to search for in column '{column_name}': ").lower()
         printValue(data, column_name, value)
+
 
 
 if __name__ == "__main__":
